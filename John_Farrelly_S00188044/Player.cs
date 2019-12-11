583bd84829fe3e629cd5dff132a12eb5bfe3f3d3
John Farrelly <S00188044@mail.itsligo.ie>
2019-12-11 09:43:47 +0000

    Add project files.

---
+++ John_Farrelly_S00188044/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace John_Farrelly_S00188044
+{
+    public enum Position { Goalkeeper, Defender,Midfielder, Forward}
+    class Player
+    {
+        
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public Position PreferredPosition { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime Age { get; set; }
+
+    }
+}
